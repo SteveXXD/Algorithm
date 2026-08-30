9d050fbb01fc049adfb20d9ae004ba303076b84a
SteveXXD <3336739310@qq.com>
2026-08-30 12:30:24 +0800

    alg

--- Pt10-Algorithm/test.py
+++ Pt10-Algorithm/test.py
@@ -1,2 +1,12 @@
-a = (1,2,3,4,5)
-b = (5,4,3,2,1)
+def edit_distance(s,t):
+    m,n = len(s),len(t)
+    prev = list(range(n+1))
+    for i in range(1,m+1):
+        cur = [i] + [0] * n
+        for j in range(1,n+1):
+            cost = 0 if s[i-1] == t[j-1] else 1
+            cur[j] = min(prev[j] +1,cur[j-1]+1,prev[j-1] + cost)
+        prev = cur
+    return prev[n]
+
+print(edit_distance("horse","ros"))
